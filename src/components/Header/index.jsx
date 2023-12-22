50ae0d5cc753ffb36a880bf5575d344411d19da9
zeteo-xulon <n.dreux@live.fr>
2023-12-22 11:00:12 +0100

    feat : beggining the structure of the front // added recharts library

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+import Nav from "../Nav"
+
+export default function Header(){
+    return (
+        <header className="header">
+            <img src="../../assets/logo_sportsee.svg" className="logo" />
+            <Nav />
+        </header>
+    )
+}
---
+++ src/components/Nav/index.jsx
@@ -0,0 +1,12 @@
+
+export default function Nav(){
+
+    return (
+        <nav className="nav">
+            <a href="#" className="nav-menu">Accueil</a>
+            <a href="#" className="nav-menu">Profil</a>
+            <a href="#" className="nav-menu">Réglage</a>
+            <a href="#" className="nav-menu">Communauté</a>
+        </nav>
+    )
+}
